b49158e84d13e3e9712f3bfd700b6855035463ef
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-08-05 18:11:07 -0300

    [master]Schemas and Resolvers, closes #2

--- app.js
+++ app.js
@@ -7,15 +7,34 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use('/graphiql', graphqlHttp({
     schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
         schema {
-            query:
-            mutation:
+            query: RootQuery
+            mutation: RootMutation
         }
     `),
-    rootValue: {}
-}));
+    rootValue: {
+        events: () => {
+            return ['Romantic cooking', 'Sailing', 'All-night coding']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+})
+
+);
 
 app.get('/', (req, res, next) => {
     res.send('Hello world!')
