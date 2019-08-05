55b068f1765ed2da845ced6c38001fe01a90b770
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-08-05 17:43:14 -0300

    [master]Project Setup e API design, closes #1

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        schema {
+            query:
+            mutation:
+        }
+    `),
+    rootValue: {}
+}));
 
 app.get('/', (req, res, next) => {
     res.send('Hello world!')
